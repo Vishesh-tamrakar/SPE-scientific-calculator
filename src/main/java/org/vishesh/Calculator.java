78ac950115115d2dc1ce44162a808d8dabf5b12b
Vishesh Tamrakar <28vishesh@gmail.com>
2026-03-06 15:31:55 +0530

    Initial Maven project setup with basic calculator functions

---
+++ src/main/java/org/vishesh/Calculator.java
@@ -0,0 +1,13 @@
+package org.vishesh;
+
+public class Calculator {
+
+    public static double add(double a, double b) {
+        return a + b;
+    }
+
+    public static double subtract(double a, double b) {
+        return a - b;
+    }
+
+}
